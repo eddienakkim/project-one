6063f6b2aef00a9908ce5577af2e4d69b691a3c7
Mauricio Gutierrez <mauriciogtrz@g.ucla.edu>
2020-05-19 20:57:32 -0700

    Testing the PokeAPI and added link to index.html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,11 @@
 //javascript
+
+var pokemon = "pikachu"
+var queryURL = "https://pokeapi.co/api/v2/pokemon/" + pokemon;
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+      console.log(response);
+  });
